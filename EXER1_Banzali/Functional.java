805edef891a1f7b9cbf5f18ee996e1ff895c6b9b
Eman M. Banzali <banzalieman758@gmail.com>
2025-09-09 21:28:19 +0800

    Add files via upload

---
+++ EXER1_Banzali/Functional.java
@@ -0,0 +1,8 @@
+public class Functional {
+    public static void main(String[] args) {
+        System.out.println("Functional: Square of first 5 numbers");
+        for(int i = 1; i <= 5; i++) {
+            System.out.println("Square of " + i + " = " + (i * i));
+        }
+    }
+}
