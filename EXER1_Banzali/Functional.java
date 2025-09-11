86ad888ba4481577adf0047251da3c938a889fe3
Eman M. Banzali <banzalieman758@gmail.com>
2025-09-11 20:17:40 +0800

    Delete EXER1_Banzali directory

--- EXER1_Banzali/Functional.java
+++
@@ -1,8 +0,0 @@
-public class Functional {
-    public static void main(String[] args) {
-        System.out.println("Functional: Square of first 5 numbers");
-        for(int i = 1; i <= 5; i++) {
-            System.out.println("Square of " + i + " = " + (i * i));
-        }
-    }
-}
